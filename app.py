6a92c57fdc008f28e697d592f77ba214f73d1901
MariaEduardaMorais <meramorais12@gmail.com>
2025-05-19 20:50:38 -0300

    Fixing bugs

--- app.py
+++ app.py
@@ -6,11 +6,12 @@ app = Flask(__name__)
 CORS(app)
 
 class Node:
-    def __init__(self, pos, distancia_percorrida=0, fe=0, caminho=None):
+    def __init__(self, pos, distancia_percorrida=0, fe=0, caminho=None, tem_fruta=False):
         self.pos = pos
         self.distancia_percorrida = distancia_percorrida
         self.fe = fe
         self.caminho = caminho if caminho else []
+        self.tem_fruta = tem_fruta
 
 def funcao_heuristica(pos_atual, pos_saida):
     return abs(pos_atual[0] - pos_saida[0]) + abs(pos_atual[1] - pos_saida[1])
@@ -25,64 +26,67 @@ def custo_movimento(cenario, pos_atual, pos_proxima, tem_fruta):
         return 1
 
 def get_min(estados):
-    if len(estados) == 0:
+    if not estados:
         return None
-    melhor_filho = estados[0]
-    for estado in estados:
-        if estado.fe < melhor_filho.fe:
-            melhor_filho = estado
-    estados.remove(melhor_filho)
-    return melhor_filho
+    melhor = min(estados, key=lambda e: e.fe)
+    estados.remove(melhor)
+    return melhor
 
-@app.route("/resolver", methods=["POST"])
+@app.route('/resolver', methods=['POST'])
 def resolver():
-    dados = request.get_json()
-    cenario = dados["cenario"]
+    data = request.get_json()
+    cenario = data['cenario']
 
     pos_inicial = None
     pos_saida = None
+
     for i in range(len(cenario)):
-        for j in range(len(cenario[i])):
+        for j in range(len(cenario[0])):
             if cenario[i][j] == 'C':
                 pos_inicial = (i, j)
             elif cenario[i][j] == 'S':
                 pos_saida = (i, j)
 
-    tem_fruta = False
+    if not pos_inicial or not pos_saida:
+        return jsonify({'erro': 'Posição inicial (C) ou final (S) não encontrada'}), 400
+
     estados = []
-    pai = Node(pos_inicial)
+    pai = Node(pos=pos_inicial)
     final = None
+    visitados = set()
 
     while True:
-        movimentos = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        movimentos = [(0,1), (1,0), (0,-1), (-1,0)]
         for dx, dy in movimentos:
             nova_pos = (pai.pos[0] + dx, pai.pos[1] + dy)
             if 0 <= nova_pos[0] < len(cenario) and 0 <= nova_pos[1] < len(cenario[0]):
-                custo = custo_movimento(cenario, pai.pos, nova_pos, tem_fruta)
+                if nova_pos in pai.caminho:
+                    continue
+                custo = custo_movimento(cenario, pai.pos, nova_pos, pai.tem_fruta)
                 if custo < float('inf'):
+                    tem_fruta_novo = pai.tem_fruta or cenario[nova_pos[0]][nova_pos[1]] == 'F'
                     filho = Node(
                         pos=nova_pos,
                         distancia_percorrida=pai.distancia_percorrida + custo,
                         fe=pai.distancia_percorrida + custo + funcao_heuristica(nova_pos, pos_saida),
-                        caminho=copy.deepcopy(pai.caminho)
+                        caminho=copy.deepcopy(pai.caminho),
+                        tem_fruta=tem_fruta_novo
                     )
                     filho.caminho.append(pai.pos)
                     estados.append(filho)
 
-                    if cenario[nova_pos[0]][nova_pos[1]] == 'F':
-                        tem_fruta = True
-
         pai = get_min(estados)
         if pai is None:
-            return jsonify({"erro": "Não foi possível encontrar um caminho até a saída."})
+            return jsonify({'mensagem': 'Não foi possível encontrar um caminho até a saída.'})
         if pai.pos == pos_saida:
             final = pai
             break
 
     return jsonify({
-        "caminho": final.caminho + [final.pos],
-        "custo": final.distancia_percorrida
+        'mensagem': 'Caminho encontrado!',
+        'caminho': final.caminho + [final.pos],
+        'custo': final.distancia_percorrida
     })
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
